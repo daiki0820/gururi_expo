693753b49d205ce51a21c8bf6fe1265d305363ad
DaikiOkada <d.okada0820@gmail.com>
2021-08-18 19:16:42 +0900

    react-native-maps導入

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import MapView from "react-native-maps";
+
+// getIntialState(){
+//   return{
+
+//   }
+// }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        initialRegion={{
+          latitude: 35.3191887,
+          longitude: 139.5467846,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        style={styles.map}
+      />
     </View>
   );
 }
@@ -14,8 +28,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
